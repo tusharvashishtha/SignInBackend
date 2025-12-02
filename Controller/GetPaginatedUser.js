c34bef083d1c2df52c300466cfd668f4fc23ec62
tushar vashishtha <vashishthatushar004@gmail.com>
2025-12-03 00:03:43 +0530

    pagination 1

---
+++ Controller/GetPaginatedUser.js
@@ -0,0 +1,6 @@
+// const User = require('../Model/User');
+
+// async funtion fetchpaginationUser(req , res){
+//     const pageSize = req.query.page_size || 10,
+//     const pageNo = req.query.page_no || 1
+// }
